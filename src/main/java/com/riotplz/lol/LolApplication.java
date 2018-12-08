ba8d59bcce6d1975e0913a0269cd346b6adfb397
tkess <tkess17@gmail.com>
2018-12-08 12:20:01 -0600

    Adding base files

---
+++ src/main/java/com/riotplz/lol/LolApplication.java
@@ -0,0 +1,12 @@
+package com.riotplz.lol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LolApplication.class, args);
+	}
+}
