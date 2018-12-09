7552d59fce9272c00cddeb47f3cc83a4cea21cd5
tkess17 <tkess17@gmail.com>
2018-12-08 19:49:02 -0600

    This did nothing

--- src/main/java/com/riotplz/controller/HomeController.java
+++ src/main/java/com/riotplz/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.riotplz.controller;
 
 import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,18 +16,16 @@ public class HomeController {
 	@Autowired
 	private RiotService riotService;
 
-	// @RequestMapping("/")
-	// public String testMethod() {
-	// System.out.println("base url is called");
-	// return "hello";
-	// }
-
 	@RequestMapping("/summoner/search")
-	public String summonerSearch(@RequestParam String summonerName) throws JSONException {
+	public String summonerSearch(@RequestParam String summonerName, Model model) throws JSONException {
 		SummonerDTO summonerDTO = riotService.getSummonerByName(summonerName);
 		if (summonerDTO == null) {
 			return "no summoner found";
+		} else {
+			model.addAttribute("found", true);
 		}
+		model.addAttribute("summoner", summonerDTO);
+		System.out.println(model);
 		return summonerDTO.getProfileIconId();
 	}
 }
